7cc31128dd63553b4db00810504d10572f617847
Chao <oleooo@126.com>
2018-07-10 15:06:00 +0800

    add prefix v

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,16 +8,16 @@ twine upload --repository-url ${PYPI_HOST} -u ${PYPI_USERNAME} -p ${PYPI_PASSWOR
 
 github-release release --user $CIRCLE_PROJECT_USERNAME \
     --repo $CIRCLE_PROJECT_REPONAME \
-    --tag `python setup.py --version`
+    --tag "v"`python setup.py --version`
 
 github-release upload --user $CIRCLE_PROJECT_USERNAME \
     --repo $CIRCLE_PROJECT_REPONAME \
-    --tag `python setup.py --version` \
+    --tag "v"`python setup.py --version` \
     --name `python setup.py --name`"-"`python setup.py --version`".tar.gz" \
     --file "dist/"`python setup.py --name`"-"`python setup.py --version`".tar.gz"
 
 github-release upload --user $CIRCLE_PROJECT_USERNAME \
     --repo $CIRCLE_PROJECT_REPONAME \
-    --tag `python setup.py --version` \
+    --tag "v"`python setup.py --version` \
     --name `python setup.py --name`"-"`python setup.py --version`"-py3-none-any.wheel" \
     --file "dist/"`python setup.py --name`"-"`python setup.py --version`"-py3-none-any.wheel"
